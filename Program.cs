a0d5273abb07dda1e5ebbc0ddccf128f8ade6878
Kirill <kiril-karp4@mail.ru>
2022-08-25 22:00:11 +1000

    День выходной?

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ else {
 
 //Задача 15: Напишите программу, которая принимает на вход цифру
 //обозначающую день недели, и проверяет, является ли этот день выходным.
-
+/*
 Console.Write("Введи цифру, обозначающую день недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
@@ -47,3 +47,4 @@ void CheckingTheDayOfTheWeek (int dayNumber) {
 }
 
 CheckingTheDayOfTheWeek(dayNumber);
+*/
