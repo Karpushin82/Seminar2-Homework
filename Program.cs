48b87d04da59306b0d99601b8ccf30d4a0b9fcf6
Kirill <kiril-karp4@mail.ru>
2022-08-25 21:43:32 +1000

    Вторая цифра трёхзначного числа

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число 
 //и на выходе показывает вторую цифру этого числа
-
+/*
 Console.Write("Введи трёхзначное число: ");
+
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
 string stringNumber = Convert.ToString(threeDigitNumber);
+
 Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+*/
