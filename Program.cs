2abeecfa31495ec0e99d4c70c9d6aec15dc070e7
Kirill <kiril-karp4@mail.ru>
2022-08-25 21:47:54 +1000

    Есть ли третья цифра

--- Program.cs
+++ Program.cs
@@ -9,3 +9,18 @@ string stringNumber = Convert.ToString(threeDigitNumber);
 
 Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
 */
+
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
